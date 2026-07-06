1427f6bafa5bf8be11648af9d489a4c7518f46a2
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-07-06 21:33:05 +0600

    add new code

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ main().then((res) => {
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email:String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+const user2 = new User({
+  name: "Eve",
+  email: "eve@gmail.com",
+  age: 30,
+});
+
+user2.save()
+.then((res) => {
+  console.log(res);
+})
+.catch((err) => {
+  console.log(err);
+});
